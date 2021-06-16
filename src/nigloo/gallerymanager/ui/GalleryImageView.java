0e6b3cff62a4b3764ef5f366e7582500bbe1e275
Niglooo <nigloo@gmx.fr>
2021-06-16 18:47:52 +0200

    Remove listener when image is loaded or there's an error

--- src/nigloo/gallerymanager/ui/GalleryImageView.java
+++ src/nigloo/gallerymanager/ui/GalleryImageView.java
@@ -3,6 +3,8 @@ package nigloo.gallerymanager.ui;
 import java.util.Objects;
 import java.util.function.Function;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.image.ImageView;
 import nigloo.gallerymanager.model.Image;
 
@@ -48,17 +50,49 @@ public class GalleryImageView extends ImageView
 					setImage(fxImage);
 				else
 				{
-					fxImage.progressProperty().addListener((obs, oldValue, newValue) ->
-					{
-						if (newValue.doubleValue() == 1)
-							setImage(fxImage);
-					});
+					new ProgressListener(this);
 				}
 			}
 			else if (fxImage != null)
 			{
 				fxImage.cancel();
 				fxImage = null;
+			}
+		}
+	}
+	
+	private static class ProgressListener implements ChangeListener<Object>
+	{
+		private final GalleryImageView imageView;
+		private final javafx.scene.image.Image fxImage;
+		
+		public ProgressListener(GalleryImageView imageView)
+		{
+			this.imageView = imageView;
+			this.fxImage = imageView.fxImage;
+			this.fxImage.progressProperty().addListener(this);
+			this.fxImage.errorProperty().addListener(this);
+		}
+		
+		@Override
+		public void changed(ObservableValue<? extends Object> observable, Object oldValue, Object newValue)
+		{
+			if (observable == fxImage.progressProperty())
+			{
+				if (((Number) newValue).doubleValue() == 1)
+				{
+					imageView.setImage(fxImage);
+					fxImage.progressProperty().removeListener(this);
+					fxImage.errorProperty().removeListener(this);
+				}
+			}
+			else if (observable == fxImage.errorProperty())
+			{
+				if ((Boolean) newValue)
+				{
+					fxImage.progressProperty().removeListener(this);
+					fxImage.errorProperty().removeListener(this);
+				}
 			}
 		}
 	}
