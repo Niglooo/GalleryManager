aaeb9ee941487c1c5e0e3c7031081a18817daf36
Niglooo <nigloo@gmx.fr>
2024-02-17 14:23:22 +0100

    Add DEILAN sort order (hard coded)

--- src/nigloo/gallerymanager/model/SortBy.java
+++ src/nigloo/gallerymanager/model/SortBy.java
@@ -2,6 +2,8 @@ package nigloo.gallerymanager.model;
 
 import java.nio.file.Path;
 import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
 
 import nigloo.gallerymanager.ui.FileSystemElement;
 import nigloo.tool.Utils;
@@ -9,11 +11,13 @@ import nigloo.tool.Utils;
 public enum SortBy
 {
 	NAME(Comparator.comparing(FileSystemElement::getPath, SortBy::compareIgnoringExtention)),
-	DATE(Comparator.comparingLong(FileSystemElement::getLastModified));
+	DATE(Comparator.comparingLong(FileSystemElement::getLastModified)),
+	DEILAN_ORDER(Comparator.comparing(FileSystemElement::getPath, SortBy::deilanOrder))
+	;
 	
 	final Comparator<FileSystemElement> comparator;
 	
-	private SortBy(Comparator<FileSystemElement> comparator)
+	SortBy(Comparator<FileSystemElement> comparator)
 	{
 		this.comparator = comparator;
 	}
@@ -39,5 +43,32 @@ public enum SortBy
 		}
 		
 		return Utils.NATURAL_ORDER.compare(p1.toString(), p2.toString());
+	}
+
+	// new type of customizable orde base on keykords
+	static private final List<String> deilanKeywords = List.of("clothed", "topless", "bottomless", "nude", "wet",  "lewd");
+
+	static private int deilanOrder(Path p1, Path p2)
+	{
+		String filename1 = p1.getFileName().toString();
+		String filename2 = p2.getFileName().toString();
+
+		int pos1 = 0;
+		for (String keyword : deilanKeywords) {
+			if (filename1.toLowerCase(Locale.ROOT).contains(keyword))
+				break;
+			pos1++;
+		}
+		int pos2 = 0;
+		for (String keyword : deilanKeywords) {
+			if (filename2.toLowerCase(Locale.ROOT).contains(keyword))
+				break;
+			pos2++;
+		}
+
+		if (pos1 == pos2)
+			return compareIgnoringExtention(p1, p2);
+		else
+			return pos1 - pos2;
 	}
 }
