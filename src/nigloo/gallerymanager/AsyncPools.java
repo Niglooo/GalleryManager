d8fa6ed740010e1a677c7dbb3360e7b89f391b56
Niglooo <nigloo@gmx.fr>
2024-02-22 22:34:07 +0100

    Change DISK_IO Executor to use virtual threads

--- src/nigloo/gallerymanager/AsyncPools.java
+++ src/nigloo/gallerymanager/AsyncPools.java
@@ -1,6 +1,7 @@
 package nigloo.gallerymanager;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 import java.util.concurrent.ForkJoinPool;
 
 import javafx.application.Platform;
@@ -8,7 +9,7 @@ import javafx.application.Platform;
 public class AsyncPools
 {
 	public static final Executor FX_APPLICATION = Platform::runLater;
-	public static final Executor DISK_IO = ForkJoinPool.commonPool();
+	public static final Executor DISK_IO = Executors.newVirtualThreadPerTaskExecutor();
 	public static final Executor HTTP_REQUEST = ForkJoinPool.commonPool();
 	public static final Executor SCRIPT_EXECUTION = ForkJoinPool.commonPool();
 	
